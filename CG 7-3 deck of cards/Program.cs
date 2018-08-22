96f71915529f99846c6214459015bc1f5ac4550c
Kristen Shea <kstar61@live.com>
2018-08-22 18:11:30 -0500

    *

--- CG 7-3 deck of cards/Program.cs
+++ CG 7-3 deck of cards/Program.cs
@@ -6,7 +6,101 @@ namespace CG_7_3_deck_of_cards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create a program that contains a deck of cards and randomly draws one.You will need a Card class to store
+            //information about each individual card.The Card class should have a method that gives you the full card name
+            //like “2 of Hearts”. You will need a Deck class that contains an array of all the cards.The Deck class will 
+            //need a method to draw a random card.Your main method in the Program class should create a deck object, draw 
+            //a random card and display the value.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+    //class Deck
+    //{
+
+    //    public int face = "1", //etc
+
+
+    //    public string suit = "Hearts"; //etc
+
+
+    //}
+
+
+    //class Deck
+    //{
+    //    private card[] cards;
+
+    //    private Deck()
+    //    {
+
+
+    //        var query =
+    //            from suit in new[] { "Spades", "Hearts", "Clubs", "Diamonds" };
+    //            from rank in Enumerable.Range(1, 13);
+    //            select new Card(rank, suit);
+
+    //        cards = query.ToArray();
+    //    }
+
+
+
+    class Card
+    {
+        public int Face { get; set; }
+        public string Suit { get; set; }
+
+
+
+        
+
+    }
+
+
+    class Deck
+    {
+        
+
+
+
+
+    }
+
+
+    
 }
