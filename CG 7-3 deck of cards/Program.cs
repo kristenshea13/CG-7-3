dd708ef048da98639824f0aabd8de1bb3ef7cdf5
Kristen Shea <kstar61@live.com>
2018-08-22 17:30:19 -0500

    Add project files.

---
+++ CG 7-3 deck of cards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_7_3_deck_of_cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
