219297e4ca8cba42040a0e0833e5b5af2c6cd6ee
Kristen Shea <kstar61@live.com>
2018-08-22 22:34:15 -0500

    deck of cards

--- CG 7-3 deck of cards/Program.cs
+++ CG 7-3 deck of cards/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CG_7_3_deck_of_cards
 {
@@ -12,25 +13,16 @@ namespace CG_7_3_deck_of_cards
             //need a method to draw a random card.Your main method in the Program class should create a deck object, draw 
             //a random card and display the value.
 
+            Console.WriteLine("Pick a card!");
+            Console.ReadLine();
 
+            Deck deck = new Deck();
 
+            Card card = deck.DrawCard();
 
+            Console.WriteLine("You drew the {0}!", card.CardName());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Console.ReadLine();
 
 
 
@@ -39,44 +31,7 @@ namespace CG_7_3_deck_of_cards
 
 
 
-
-
-
-
-
-
     }
-
-
-
-
-    //class Deck
-    //{
-
-    //    public int face = "1", //etc
-
-
-    //    public string suit = "Hearts"; //etc
-
-
-    //}
-
-
-    //class Deck
-    //{
-    //    private card[] cards;
-
-    //    private Deck()
-    //    {
-
-
-    //        var query =
-    //            from suit in new[] { "Spades", "Hearts", "Clubs", "Diamonds" };
-    //            from rank in Enumerable.Range(1, 13);
-    //            select new Card(rank, suit);
-
-    //        cards = query.ToArray();
- 
 
 
 
@@ -84,21 +39,80 @@ namespace CG_7_3_deck_of_cards
     {
         public int Face { get; set; }
         public string Suit { get; set; }
-        public string Color { get; set; }
+        
+
+        public string CardName()
+        {
+            string faceValue = "";
+            if (Face >= 2 && Face <= 10)
+                faceValue = Face.ToString();
+            else if (Face == 1)
+                faceValue = "Ace";
+            else if (Face == 11)
+                faceValue = "Jack";
+            else if (Face == 12)
+                faceValue = "Queen";
+            else if (Face == 13)
+                faceValue = "King";
+
+
+            return $"{faceValue} of {Suit}";
+        }
+
 
 
     }
+
 
 
     public class Deck
     {
+
         //property called Cards
-        //deckArray
+        //deck list
         //random card draw
+        public List<Card> Deckofcards { get; set; }
+
+        //constructor- to make sure the deck is always created and cannot draw a card before there is a deck
+        public Deck()
+        {
+            string[] suits = { "Hearts", "Clubs", "Diamonds", "Spades" };
+
+            Deckofcards = new List<Card>();
+
+            foreach (var suit in suits)
+            {
+                for (int face = 1; face <= 13; face++)
+                {
+                    Card card = new Card
+                    {
+                        Face = face,
+                        Suit = suit
+                    };
+
+                    Deckofcards.Add(card);
+
+                }
+
+
+            }
+
+
+        }
+
+        public Card DrawCard()
+        {
+            Random rnd = new Random();
+            int cardIndex = rnd.Next(0, 52);
+
+            return Deckofcards[cardIndex];
+
+        }
+
 
 
     }
 
 
-    
+
 }
